bd3a36275cc3f4a7d232ffd0cef9e89b4a50119e
ZeroisKT <zerois_1996@hotmail.com>
2023-01-24 11:59:24 +0000

    Changes_observer_and_event

--- TresdTech/FinalProject/Observer/CustomerLogin.php
+++ TresdTech/FinalProject/Observer/CustomerLogin.php
@@ -7,11 +7,13 @@ class CustomerLogin implements ObserverInterface
 {
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        
         $customer = $observer->getEvent()->getCustomer();
+        $layout = $observer->getEvent()->getLayout();
         $email = $customer->getEmail(); //Get customer Email
         $name = $customer->getName(); //Get customer Name
-        echo "Welcome ".$name;
+        $message = "Bienvenido ".$name;
+        $this->messageManager->addSuccessMessage(__($message));
+        $layout->setName($message);
         exit;
     }
 }
