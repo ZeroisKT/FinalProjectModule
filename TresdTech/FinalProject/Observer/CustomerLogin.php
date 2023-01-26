8996530f3b78c21c906868ee27744f18680c0b0f
ZeroisKT <zerois_1996@hotmail.com>
2023-01-26 00:42:55 +0000

    bug_observer_corrected

--- TresdTech/FinalProject/Observer/CustomerLogin.php
+++ TresdTech/FinalProject/Observer/CustomerLogin.php
@@ -2,19 +2,25 @@
 namespace TresdTech\FinalProject\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Message\ManagerInterface;
 
 class CustomerLogin implements ObserverInterface
 {
+    public function __construct(   
+        ManagerInterface $messageManager
+    )
+    {   
+        $this->messageManager = $messageManager;
+    }
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $customer = $observer->getEvent()->getCustomer();
-        $layout = $observer->getEvent()->getLayout();
         $email = $customer->getEmail(); //Get customer Email
         $name = $customer->getName(); //Get customer Name
         $message = "Bienvenido ".$name;
         $this->messageManager->addSuccessMessage(__($message));
-        $layout->setName($message);
-        exit;
+        
     }
 }
 ?>
